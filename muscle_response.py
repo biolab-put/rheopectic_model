5ce0d1c49b3369c2374fd8c03d45d46eb15e92b5
Jakubach <jakubach@gmail.com>
2021-09-03 12:40:52 +0200

    Kod pozwalający na estymacje z optymalizacją parametrów modelu Hilla z reopeksją

--- muscle_response.py
+++ muscle_response.py
@@ -1,22 +1,21 @@
-from re import U
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy import signal
-import pandas as pd
-from scipy.optimize import minimize
-import math
 from scipy.integrate import solve_ivp
-from scipy.signal import cont2discrete, lti, dlti, dstep
-from scipy.ndimage.interpolation import shift
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
-def objective(x, X0, time_vector, active_force, sim_dt, reference_force):
-    km = x[0]
-    kt = x[1]
-    m = x[2]
-    c = x[3]
-    muscle_data = muscle_response(X0, time_vector, active_force,m,km,kt,c,sim_dt)
+def objective(x, X0, time_vector, active_force,m,km,kt,k2,A,D,max_c,c0, sim_dt, reference_force):
+    min_c = x[0]
+    k1 = x[1]
+    B = x[2]
+    C = x[3]
+    muscle_data =  rheopectic_muscle_response(X0,time_vector, active_force,m,km,kt,k1,k2, A,B,C,D, max_c, min_c,c0, sim_dt)
     estimated_force = pd.DataFrame(data = {'timestamp': time_vector,'estimated force' : muscle_data['estimated force']})
     return np.sum((estimated_force['estimated force']-reference_force['reference force'])**2)
+
+def load_data(filename):
+    data = pd.read_csv(filename)
+    return data
 
 def parabolic_twitch(t,twitch_duration,twitch_delay,twitch_amplitude, twitch_frequency, sim_dt):
     single_twitch_t = np.arange(0,twitch_duration,sim_dt)
@@ -28,53 +27,49 @@ def parabolic_twitch(t,twitch_duration,twitch_delay,twitch_amplitude, twitch_fre
     twitch_train = np.roll(twitch_train,int((twitch_delay)/sim_dt))
     twitch_train[0:int((twitch_delay+sim_dt)/sim_dt)] = 0
     return twitch_train
-    #print(pulse)
-    #pulse[int((twitch_delay+twitch_duration)/sim_dt)::] = 0
-    #return pulse
-    #train_twitch_t = twitch_delay:1/twitch_frequency:t(end)
-    #np.arange(twitch_delay,1/twitch_frequency,sim_dt)
 
-def muscle_response(X0,time_vector, active_force,m,km,kt,c,sim_dt):
-    a = np.array([[0, 1], [(-abs(km/m)-abs(kt/m)), -abs(c/m)]])
-    b = np.array([[0], [abs(1/m)]])
-    #change from lm to force and convert to mN
-    c = np.array([[abs(kt)*1000, 0]])
-    d = np.array([[0]])
-    sys = signal.StateSpace(a, b, c, d)
-    sys = sys.to_discrete(sim_dt)
-    #tout, yout, xout = signal.lsim(system = sys,U = active_force, T = time_vector,X0 = X0)
-    tout, yout, xout = signal.dlsim(system = sys,u = active_force, t = time_vector,x0 = X0)
-    return pd.DataFrame(data = {'timestamp': tout, 'estimated force': yout[:,0], 'lm' : xout[:,0], 'dlm': xout[:,1]})
+def solve_muscle_dynamics(t,x,active_force, m, km,kt,k1,k2, A,B,C,D, max_c, min_c,c0, sim_dt):
+    lm, dlm, Lambda = x
+    Lambda = np.max([0.0, np.min([1.0, Lambda])])
+    #dx_dt[0] = dlm
+    dLambda = -k1*np.sign(dlm)*(np.abs(dlm)**(A))*(Lambda**(B))+k2*np.sign(dlm)*(np.abs(dlm)**(C))*((1-Lambda)**(D))
+    K = 1 - np.sqrt(max_c/min_c)
+    c = max_c/((1 - K * Lambda)**2) +c0
+    d2lm = 1/m * (-np.abs(c)*dlm-np.abs(km)*lm-np.abs(kt)*lm+active_force[int(t/sim_dt)])
+    return [dlm,d2lm,dLambda]
 
-def load_data(filename):
-    data = pd.read_csv(filename)
-    return data
+
+def rheopectic_muscle_response(X0,time_vector, active_force,m,km,kt,k1,k2, A,B,C,D, max_c, min_c,c0, sim_dt):
+    solution = solve_ivp(solve_muscle_dynamics, t_span=[0, time_vector[-1]], y0=X0, t_eval = time_vector, args=(active_force, m, km,kt,k1,k2, A,B,C,D, max_c, min_c,c0, sim_dt), method = 'LSODA',first_step = sim_dt, max_step = sim_dt)
+    estimated_force = np.abs(kt) * 1000 * solution.y[0,:]
+    return pd.DataFrame(data = {'timestamp': time_vector, 'estimated force': estimated_force, 'lm' : solution.y[0,:], 'dlm': solution.y[1,:]})
+
 
 def muscle_state():
     sim_dt = 0.001
     sim_time = 5.1
-    zeros_time = 1.2 - 0.5 #sim_time - 0.5
     time_vector = np.arange(0,sim_time,sim_dt)
     twitch_duration = 0.025
     twitch_delay = 0
     twitch_frequency = 4
     twitch_amplitude = 10
-    km = 0.1848                   
-    kt = 0.0087
-    m = 0.0038
-    c =  0.0350  
-    X0 = [0, 0]
-
-
-    #active_force = twitch_amplitude * signal.unit_impulse(len(time_vector), (np.arange(twitch_delay+sim_dt,twitch_delay+twitch_duration,sim_dt)/sim_dt).astype(int))
- 
+    k1 = 5
+    k2 = 1
+    A = 1
+    B = 1
+    C = 2
+    D = 1
+    max_c = 1.6
+    min_c = 0.03
+    X0 = [0,0,0] #lm, dlm, lambda
+    km = 0.1733323                   
+    kt = 0.02612695
+    m = 0.0085757
+    c0 = 0 
 
     filenames = ['1006j_trial_0', '1006j_trial_5', '1006j_trial_0']
-    
-    
-
     reference_data = [load_data(f'./{filename}') for filename in filenames]
-    zeros = pd.DataFrame(np.zeros((2000,reference_data[0].shape[1])),columns=reference_data[0].columns)
+    zeros = pd.DataFrame(np.zeros((2000+4000,reference_data[0].shape[1])),columns=reference_data[0].columns)
     reference_data[0]['force'] = reference_data[0]['force'] - reference_data[0]['force'][0]
     reference_data[2]['force'] = reference_data[2]['force'] - reference_data[2]['force'][0]
     reference_data[0] = reference_data[0][0:600]
@@ -99,42 +94,25 @@ def muscle_state():
 
     plt.plot(time_vector, active_force)
     plt.plot(time_vector, reference_data['stimDig'])
-
-    #plt.show()
-    #reference_data = reference_data[reference_data['timestamp'] <= sim_time]
-    #reference_data['force'][int(zeros_time/sim_dt)::] = 0
-    #reference_force = pd.DataFrame(data = {'timestamp': time_vector, 'reference force' : reference_data['force'][0:int(sim_time/sim_dt)]})
-    #reference_force['reference force'] = (refere
-    # reference_force['reference force'] - reference_force['reference force'][0])
-    #reference_force['reference force'][int(zeros_time/sim_dt)::] = 0
-    
-    #plt.plot(time_vector, active_force)
-    #plt.show()
-
-    #reference_force.plot(x='timestamp', y="reference force")
-
-    plt.plot(time_vector, reference_data['force'])
-    #plt.show()
-
-    estimated_muscle_data = muscle_response(X0,time_vector, active_force,m,km,kt,c,sim_dt)
+    plt.show()
+    #estimated_muscle_data = rheopectic_muscle_response(X0,time_vector, active_force,m,km,kt,k1,k2, A,B,C,D, max_c, min_c,c0, sim_dt)
     #estimated_muscle_data.plot(x="timestamp", y="estimated force")
+    #print(test)
+    #plt.plot(test)
     #plt.show()
-    x0 = [km,kt,m,c]
     #reference_force = reference_data['force']
     reference_force = pd.DataFrame(data = {'timestamp': time_vector, 'reference force' : reference_data['force']})
-    result = minimize(objective, x0, args = (X0, time_vector, active_force, sim_dt, reference_force), method='L-BFGS-B',bounds = ((0, None), (0, None),(0, None),(0, None)))#, jac=derivative)
-    fitted_muscle_data = muscle_response(X0,time_vector, active_force,result.x[2],result.x[0],result.x[1],result.x[3],sim_dt)
-    
-
-    #ax = fitted_muscle_data.plot(x="timestamp", y="estimated force")
+    x0 = [min_c,k1,B,C]
+    result = minimize(objective, x0, args = (X0, time_vector, active_force,m,km,kt,k2,A,D,max_c,c0, sim_dt, reference_force), method='L-BFGS-B',bounds = ((0.001, None), (0, None),(0, None),(0, None)),options={'iprint':-1})#, jac=derivative)
+    fitted_muscle_data = rheopectic_muscle_response(X0,time_vector, active_force,m,km,kt,result.x[1],k2, A,result.x[2],result.x[3],D, max_c, result.x[0],c0, sim_dt)
     plt.plot(time_vector, fitted_muscle_data['estimated force'])
     plt.legend(['Active force', 'Distribution of stimuli', 'Reference muscle force', 'Estimated muscle force'],loc ='upper right')
     plt.xlabel('Time [s]')
-    plt.ylabel('Force [N]')
+    plt.ylabel('Force [mN]')
     #reference_force.plot(x='timestamp', y="reference force",ax=ax)
     print(result)
     plt.show()
-    
+
 
 
 muscle_state()

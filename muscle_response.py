7fd3fd22de20c369a38a0ab88ffa9f8e5bcda163
Jakubach <jakubach@gmail.com>
2021-12-08 16:48:29 +0100

    update of active force generator

--- muscle_response.py
+++ muscle_response.py
@@ -29,13 +29,17 @@ def load_data(filename):
 
 def parabolic_twitch(t,twitch_duration,twitch_delay,twitch_amplitude, twitch_frequency, sim_dt):
     single_twitch_t = np.arange(0,twitch_duration,sim_dt)
-    pulse = np.zeros(int(1/(sim_dt*twitch_frequency)))
-    pulse[(single_twitch_t/sim_dt).astype(np.int8)] = twitch_amplitude * np.sin(np.pi * single_twitch_t / twitch_duration)
+    pulse = twitch_amplitude * np.sin(np.pi * single_twitch_t / twitch_duration)
+    zeros_duration = (1/twitch_frequency) - twitch_duration
+    assert zeros_duration > 0, "Twitch duration should be smaller than the inverse of it's frequency (period)."
+    zeros = np.zeros(np.int32(zeros_duration/sim_dt))
+    pulse = np.concatenate((pulse,zeros))
+    #pulse = np.pad(pulse, (0, np.int32(((1/twitch_frequency) - twitch_duration)/sim_dt)), 'constant')
     repeat = np.ceil((t[-1]+sim_dt)/(1/twitch_frequency))
     twitch_train = np.tile(pulse, int(repeat))
     twitch_train = twitch_train[0:len(t)]
     twitch_train = np.roll(twitch_train,int((twitch_delay)/sim_dt))
-    twitch_train[0:int((twitch_delay+sim_dt)/sim_dt)] = 0
+    twitch_train[0:np.int32((twitch_delay+sim_dt)/sim_dt)] = 0
     return twitch_train
 
 def solve_muscle_dynamics(t,x,active_force, m, km,kt,k1,k2, A,B,C,D, max_c, min_c,c0, sim_dt):

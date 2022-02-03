39c0b1025170100ff348b947c898a8a72c161a41
Jakubach <jakubach@gmail.com>
2022-02-03 15:25:11 +0100

    Update hill_muscle_model.py

--- object-oriented/hill_muscle_model.py
+++ object-oriented/hill_muscle_model.py
@@ -1,22 +1,27 @@
 from scipy.integrate import ode
 import numpy as np
+from matplotlib import pyplot as plt 
 
 class hill_muscle_model:
-    def __init__(self,km,kt,m,c,F_K,F_delta,delta,sim_dt) -> None:
+    def __init__(self,km,kt,m,c,delta,sim_dt) -> None:
     #def __init__(self,km,kt,m,c,delta,sim_dt) -> None:
         self.km = km
         self.kt = kt
         self.m = m
         self.c = c
-        self.F_K = F_K
-        self.F_delta = F_delta
+        #self.F_K = F_K
+        #self.F_delta = F_delta
         self.delta = delta
         self.sim_dt = sim_dt
 
     def _solve_muscle_dynamics(self,t,X,active_force):
-        lm, dlm_dt = X
-        d2lm_dt = 1/self.m*(-self.c*dlm_dt-self.km*lm+self.kt*(self.delta-lm)-active_force[int(t/self.sim_dt)])
-        return [dlm_dt,d2lm_dt]
+        lm, dlm_dt,Fact = X
+        T = 0.5
+        K = 1
+        dFact_dt = 1/T * (K * active_force[int(t/self.sim_dt)] - Fact)
+        d2lm_dt = 1/self.m*(-self.c*dlm_dt-self.km*lm+self.kt*(self.delta-lm)-Fact)
+        return [dlm_dt,d2lm_dt,dFact_dt]
+
 
     def muscle_response(self,X0,time_vector,active_force):
         solver = ode(self._solve_muscle_dynamics)
@@ -29,13 +34,9 @@ class hill_muscle_model:
             solver.integrate(time_vector[n])
             assert solver.successful()
             solution[:,n] = solver.y
-            #solution[2,n] = np.max([0.0, np.min([1.0, solution[2,n]])])
         estimated_force = np.sign(self.delta - solution[0,:]) * np.abs(self.delta - solution[0,:]) * 10
-        #estimated_force = self.kt * np.sign(self.delta - solution[0,:]) * np.abs(self.delta - solution[0,:])
+
         estimated_force = estimated_force - estimated_force[0]
-        #estimated_force = 1 / (1 + np.exp(-self.F_K*(estimated_force-self.F_delta)))
-        
-        #estimated_force = estimated_force - estimated_force[0]
         return estimated_force,solution
 
     '''
@@ -55,21 +56,21 @@ class hill_muscle_model:
         estimated_force = estimated_force - estimated_force[0]
         return estimated_force
     '''
-    def set_parameters(self,km,kt,m,c,F_K,F_delta):
+    def set_parameters(self,km,kt,m,c):
         self.km = km
         self.kt = kt
         self.m = m
         self.c = c
-        self.F_K = F_K
-        self.F_delta = F_delta
+        #self.F_K = F_K
+        #self.F_delta = F_delta
 
     def set_parameters(self,x):
         self.km = x[0]
         self.kt = x[1]
         self.m = x[2]
         self.c = x[3]
-        self.F_K = x[4]
-        self.F_delta = x[5]
+        #self.F_K = x[4]
+        #self.F_delta = x[5]
     
     def set_parameter(self, parameter, value):
         old_parameter_value = getattr(self, parameter)
@@ -77,8 +78,8 @@ class hill_muscle_model:
         return old_parameter_value
 
     def get_parameters(self):
-        x = [self.km,self.kt,self.m,self.c,self.F_K,self.F_delta]
-        #x = [self.km,self.kt,self.m,self.c]
+        #x = [self.km,self.kt,self.m,self.c,self.F_K,self.F_delta]
+        x = [self.km,self.kt,self.m,self.c]
         return x
 
     @staticmethod
@@ -102,13 +103,13 @@ class hill_muscle_model:
 
     def get_X0(self):
         lm0 = self.get_initial_length()
-        X0 = [lm0,0]
+        X0 = [lm0,0,0]
         return X0
 
 class rheopectic_hill_muscle_model(hill_muscle_model):
     # TODO: OVERWRITE METHODS
     def __init__(self, km,kt,m,c0,min_c,max_c,k1,k2,A,B,C,D,lambda0,delta,sim_dt):
-        super().__init__(km = km,kt = kt,m = m,c=0, F_K = 0, F_delta = 0, delta = delta, sim_dt = sim_dt)
+        super().__init__(km = km,kt = kt,m = m,c=0, delta = delta, sim_dt = sim_dt)
         self.k1 = k1
         self.k2 = k2
         self.A = A
@@ -119,11 +120,35 @@ class rheopectic_hill_muscle_model(hill_muscle_model):
         self.c0 = c0
         self.min_c = min_c
         self.max_c = max_c
+    
+    def Lambda2viscosity(self,Lambda):
+        #Lambda = 0
+        max_c = self.max_c
+        min_c = self.min_c
+        if(self.C > self.A):
+            max_c = self.min_c
+            min_c = self.max_c
+
+        K = 1 - np.sqrt(max_c/min_c)
+        c = max_c/((1 - K * Lambda)**2) + self.c0
+        #print(c)
+        #exit()
+        #else:
+        #    K = 1 - np.sqrt(self.max_c/self.min_c)
+        #    c = self.max_c/((1 - K * Lambda)**2) + self.c0
+        #print(c)
+        #exit()
+        return c
 
     def _solve_muscle_dynamics(self,t,X,active_force):
         lm, dlm_dt, Lambda = X
-        #Lambda = np.max([0.0, np.min([1.0, Lambda])])
-        dLambda_dt = -self.k1*np.sign(dlm_dt)*(np.abs(dlm_dt)**(self.A))*(Lambda**(self.B))+self.k2*np.sign(dlm_dt)*(np.abs(dlm_dt)**(self.C))*((1-Lambda)**(self.D))
+        Lambda_temp = np.max([0.0, np.min([1.0, Lambda])])
+        #if(self.k2*np.sign(dlm_dt)*(np.abs(dlm_dt)**(self.C))*((1-Lambda)**(self.D)) < 0):
+        #    print('down', -self.k1*np.sign(dlm_dt)*(np.abs(dlm_dt)**(self.A))*(Lambda**(self.B)))
+        #    print('up', self.k2*np.sign(dlm_dt)*(np.abs(dlm_dt)**(self.C))*((1-Lambda)**(self.D)))
+
+        dLambda_dt = -self.k1*np.sign(dlm_dt)*((1.0 + np.abs(dlm_dt))**(self.A) - 1)*(Lambda_temp**(self.B))+self.k2*np.sign(dlm_dt)*((1.0 + np.abs(dlm_dt))**(self.C)-1)*((1-Lambda_temp)**(self.D))
+        #dLambda_dt = -self.k1*np.sign(dlm_dt)*((np.abs(dlm_dt))**(self.A))*(Lambda_temp**(self.B))+self.k2*np.sign(dlm_dt)*((np.abs(dlm_dt))**(self.C))*((1-Lambda_temp)**(self.D))
         if(Lambda >= 1.0 and dLambda_dt > 0.0):
             Lambda = 1.0
             dLambda_dt = 0.0
@@ -131,18 +156,22 @@ class rheopectic_hill_muscle_model(hill_muscle_model):
             Lambda = 0.0
             dLambda_dt = 0.0
         #print(np.abs(dlm_dt))
-        K = 1 - np.sqrt(self.max_c/self.min_c)
-        c = self.max_c/((1 - K * Lambda)**2) + self.c0
+        c = self.Lambda2viscosity(Lambda)
+        #K = 1 - np.sqrt(self.min_c/self.max_c)
+        #c = self.min_c/((1 - K * Lambda)**2) + self.c0
         #print(c)
 
         #Fd = -c*dlm_dt
         #Fd = Lambda * 1 + Lambda * self.max_c * dlm_dt + self.min_c*dlm_dt
         d2lm_dt = 1/self.m*(-c*dlm_dt-self.km*lm+self.kt*(self.delta-lm)-active_force[int(t/self.sim_dt)])
+        #d2lm_dt = 1/self.m*(-c*dlm_dt-(np.sign(lm)*self.km*lm**2+self.km*lm)+(np.sign((self.delta-lm))*self.kt*(self.delta-lm)**2 + self.kt*(self.delta-lm))-active_force[int(t/self.sim_dt)])
+        #d2lm_dt = 1/self.m*(-c*dlm_dt-(np.exp(self.km*lm)-1)+(np.exp(self.kt*(self.delta-lm))-1)-active_force[int(t/self.sim_dt)])
         return [dlm_dt,d2lm_dt,dLambda_dt]
 
-    def set_parameters(self,min_c,k1,C,D,c0,lambda0):
+    def set_parameters(self,min_c,k1,k2,C,D,c0,lambda0):
         self.min_c = min_c
         self.k1 = k1
+        self.k2 = k2
         self.C = C
         self.D = D
         self.c0 = c0
@@ -151,14 +180,15 @@ class rheopectic_hill_muscle_model(hill_muscle_model):
     def set_parameters(self,x):
         self.min_c = x[0]
         self.k1 = x[1]
-        self.C = x[2]
-        self.D = x[3]
-        self.c0 = x[4]
-        self.lambda0 = x[5]
-        self.max_c = x[6]
+        self.k2 = x[2]
+        self.C = x[3]
+        self.D = x[4]
+        self.c0 = x[5]
+        self.lambda0 = x[6]
+        self.max_c = x[7]
 
     def get_parameters(self):
-        x = [self.min_c,self.k1,self.C,self.D,self.c0,self.lambda0,self.max_c]
+        x = [self.min_c,self.k1,self.k2,self.C,self.D,self.c0,self.lambda0,self.max_c]
         return x
 
     def get_X0(self):

190b8e727d9bf7a8fc659cbb507bd231e66a67c9
Jakubach <jakubach@gmail.com>
2022-08-22 17:22:20 +0200

    clean code

--- object-oriented/muscle_active_force.py
+++ object-oriented/muscle_active_force.py
@@ -1,9 +1,4 @@
-from re import M
 import numpy as np
-from matplotlib import pyplot as plt 
-
-def inverse_rounding(number):
-    return int(np.round(np.trunc(number)+ (1-np.mod(number, 1.0))))
 
 class muscle_active_force:
         @staticmethod
@@ -51,28 +46,4 @@ class muscle_active_force:
             single_pulse[np.arange(a/sim_dt,b/sim_dt,1,dtype = int)] = twitch_amplitude
             pulse[0:len(single_twitch_t)] = single_pulse
             twitch_train = cls._generate_wave(t,pulse,twitch_delay, twitch_frequency, sim_dt)
-            return twitch_train
-        @staticmethod
-        def sigmoid_twitch(t,twitch_duration,twitch_delay,twitch_amplitude, twitch_frequency, sim_dt):
-            single_twitch_t = np.arange(0,twitch_duration+twitch_delay,sim_dt)
-            #pulse =  twitch_amplitude*((1/ (1+np.exp(-10000*(single_twitch_t)))) + (0.5/ (1+np.exp(1000*(single_twitch_t-twitch_duration)))))-twitch_amplitude
-            pulse = twitch_amplitude * (1/(1+np.exp(-10000*(single_twitch_t-twitch_delay-twitch_delay/2.5)))) * (1/(1+(np.exp(500*(single_twitch_t-twitch_duration-twitch_delay)))))
-            #pulse = pulse[pulse>=0.75*twitch_amplitude]
-            plt.plot(pulse)
-            plt.show()
-            exit()
-
-        def exp_twitch(t,twitch_duration,twitch_delay,twitch_amplitude, twitch_frequency, sim_dt):
-            single_twitch_t = np.arange(0,twitch_duration,sim_dt)
-            pulse = (1 - np.exp(-single_twitch_t/0.00005))*(1/(1+np.exp(700*(single_twitch_t-twitch_duration/10))))
-            #pulse = (1 - np.exp(-single_twitch_t/0.00005))*(1/(1+np.exp(1900*(single_twitch_t-twitch_duration/2))))
-            zeros_duration = (1/twitch_frequency) - twitch_duration
-            assert zeros_duration > 0, "Twitch duration should be smaller than the inverse of it's frequency (period)."
-            zeros = np.zeros(np.int32(zeros_duration/sim_dt))
-            pulse = np.concatenate((pulse,zeros))
-            repeat = np.ceil((t[-1]+sim_dt)/(1/twitch_frequency))
-            twitch_train = np.tile(pulse, int(repeat))
-            twitch_train = twitch_train[0:len(t)]
-            twitch_train = np.roll(twitch_train,int((twitch_delay)/sim_dt))
-            twitch_train[0:np.int32((twitch_delay+sim_dt)/sim_dt)] = 0
             return twitch_train

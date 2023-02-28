119c75e4cfb5ba74cb6e6de48bc3e36151fda279
Jakubach <jakubach@gmail.com>
2023-02-28 11:34:31 +0100

    Model refactorisation and added parameters to the model (A,B,C,D)

--- py_simple_rheopectic_model/rheological_muscle_model.py
+++ py_simple_rheopectic_model/rheological_muscle_model.py
@@ -1,10 +1,8 @@
 from scipy.integrate import ode
 import numpy as np
 
-
 class RheopecticMuscle():
- # TODO: OVERWRITE METHODS
-    def __init__(self, km,kt,m,cs,ks,ls0,c_rh,c_rh_min,c1,k1,k2,lambda0,F0,s1,delta,sim_dt):
+    def __init__(self, km,kt,m,cs,ks,ls0,c_rh,c_rh_min,c1,k1,k2,lambda0,F0,s1,A,B,C,D, delta,sim_dt):
         self.k1 = k1
         self.k2 = k2
         self.m = m
@@ -21,27 +19,25 @@ class RheopecticMuscle():
         self.s1 = s1
         self.delta = delta
         self.sim_dt = sim_dt
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
 
     def _get_dls_dt(self, current_ls, current_active_force):
         dls_dt = 1/self.cs * (-(self.ks * current_ls) + current_active_force)
         return dls_dt
 
+
     def _solve_muscle_dynamics(self,t,X,active_force):
         lm, dlm_dt, Lambda,ls = X
         Lambda = np.clip(Lambda,0,1)
-        #dlm_dt_temp = np.max([0,dlm_dt])
-
         dGamma_dt = np.max([0,dlm_dt]) / self.s1
-
-        #print( - self.k1 * (Lambda), self.k2 * dGamma_dt * (1-Lambda) )
-        dLambda_dt = - self.k1 * (Lambda) + self.k2 * dGamma_dt * (1-Lambda)
-        #if(dLambda_dt < 1e-1):
-        #    Lambda = ((self.k2 * dGamma_dt) / self.k1) / ( 1 + ((self.k2 * dGamma_dt) / self.k1))
-        rheopectic_force = self.c_rh * dGamma_dt * (self.s1 ** 2) * (Lambda)
-        #dls_dt = 1/self.cs * (-(self.ks * ls) + active_force[int(t/self.sim_dt)])
+        dLambda_dt = - self.k1 * np.power(dGamma_dt,self.A) * np.power(Lambda,self.B) + self.k2 * np.power(dGamma_dt,self.C) * np.power(1-Lambda,self.D)
+        rheopectic_force = self.c_rh * dGamma_dt * (np.square(self.s1)) * (Lambda)
         dls_dt = self._get_dls_dt(ls,active_force[int(t/self.sim_dt)])
-        d2lm_dt = 1/self.m*(-(rheopectic_force + self.c_rh_min * dlm_dt + self.c1 * dls_dt) - self.km*(np.sign(lm) * (lm**2))+self.kt*(np.sign(self.delta-lm)* ((self.delta-lm)**2))-active_force[int(t/self.sim_dt)] - self.F0)
-        #d2lm_dt = 1/self.m*(-self.c_rh/5*dlm_dt-self.km*(lm**2)+self.kt*((self.delta-lm)**2)-active_force[int(t/self.sim_dt)])
+        d2lm_dt = 1/self.m*(-(rheopectic_force + self.c_rh_min * dlm_dt + self.c1 * dls_dt) - self.km*(np.sign(lm) * np.square(lm))+self.kt*(np.sign(self.delta-lm)* (np.square(self.delta - lm)))-active_force[int(t/self.sim_dt)] - self.F0)
+
         return [dlm_dt,d2lm_dt,dLambda_dt,dls_dt]
 
     def muscle_response(self,X0,time_vector,active_force):
@@ -52,20 +48,21 @@ class RheopecticMuscle():
         solution = np.zeros((len(X0),len(time_vector)))
         solution[:,0] = X0
         for n in range(1,len(time_vector)):
-            try:
+            try:   
                 solver.integrate(time_vector[n])
                 assert solver.successful()
                 solution[:,n] = solver.y
             except AssertionError as e:
                 solution = np.zeros((len(X0),len(time_vector)))
-
-        estimated_force = self.kt * np.sign(self.delta - solution[0,:]) * (np.abs(self.delta - solution[0,:]) **2) * 0.1
+                break
+        estimated_force = self.kt * np.sign(self.delta - solution[0,:]) * (np.square(np.abs(self.delta - solution[0,:]))) * 0.1
         #estimated_force = self.kt * (np.sign(self.delta - solution[0,:]) * (np.abs(self.delta - solution[0,:]) ** (1)) + np.sign(self.delta - solution[0,:]) * (np.abs(self.delta - solution[0,:]) ** (1))) * 10
         estimated_force = estimated_force - estimated_force[0]
         estimated_force = np.clip(estimated_force,0,None)
         return estimated_force,solution
 
     def set_parameters(self,x):
+        '''
         self.km = x[0]
         self.kt = x[1]
         self.m = x[2]
@@ -80,36 +77,48 @@ class RheopecticMuscle():
         self.lambda0 = x[11]
         self.F0 = x[12]
         self.s1 = x[13]
+        self.A = x[14]
+        self.B = x[15]
+        self.C = x[16]
+        self.D = x[17]
+        '''
+        self.F0 = x[0]
+        self.ls0 = x[1]
+        self.c_rh = x[2]
+        self.k1 = x[3]
+        self.k2 = x[4]
+        self.A = x[5]
+        self.B = x[6]
+        self.C = x[7]
+        self.D = x[8]
+        self.cs = x[9]
+        self.ks = x[10]
 
     def get_parameters(self):
-        x = [self.km, self.kt,self.m ,self.cs,self.ks,self.ls0,self.c_rh,self.c_rh_min,self.c1,self.k1,self.k2,self.lambda0,self.F0,self.s1]
+        #x = [self.km, self.kt,self.m ,self.cs,self.ks,self.ls0,self.c_rh,self.c_rh_min,self.c1,self.k1,self.k2,self.lambda0,self.F0,self.s1,self.A,self.B,self.C,self.D]
+        x = [self.F0,self.ls0, self.c_rh, self.k1 ,self.k2,self.A ,self.B,self.C,self.D,self.cs,self.ks]
         return x
 
     def get_initial_length(self):
         dls0 = self._get_dls_dt(self.ls0,0)
-
-        #a = self.km + self.kt
         a = self.kt - self.km
         b = 2 * self.kt * self.delta
-        c = self.kt*(self.delta**2) - self.c1 * dls0 - self.F0
-        quadratic_discriminant = b**2 - 4 * a * c
-        lm01 = (-b + quadratic_discriminant**(1/2)) / (2 * a)
-        lm02 = (-b - quadratic_discriminant**(1/2)) / (2 * a)
+        c = self.kt*(np.square(self.delta)) - self.c1 * dls0 - self.F0
+        quadratic_discriminant = np.square(b) - 4 * a * c
+        lm01 = (-b + np.sqrt(quadratic_discriminant)) / (2 * a)
+        lm02 = (-b - np.sqrt(quadratic_discriminant)) / (2 * a)
         if((lm01 > 0 and lm02 > 0) or (lm01 < 0 and lm02 < 0)):
             return False
         if (lm01 > 0):
             return lm01
         if (lm02 > 0):
             return lm02
-        #return (self.kt * self.delta - self.c1 * dls0) / (self.kt + self.km)
-        #return (self.kt * self.delta - self.F0) / (self.kt + self.km)
-        #return (self.kt * self.delta - self.F0 - self.c2 * self.lambda0) / (self.kt + self.km)
 
     def get_X0(self):
         lm0 = self.get_initial_length()
         X0 = [lm0,0,self.lambda0,self.ls0]
         return X0
-       
+
     @staticmethod
     def get_stiffness_ratio(x):
         km = x[0]
